5d56269b31b450a4421eb78ee83fe2e77214ad17
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2024-12-19 19:24:58 +0100

    Add GroupData deserialization

--- plugin/src/main/java/fr/flowsqy/easytab/EasyTabPlugin.java
+++ plugin/src/main/java/fr/flowsqy/easytab/EasyTabPlugin.java
@@ -1,11 +1,29 @@
 package fr.flowsqy.easytab;
 
+import java.util.Map;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.flowsqy.easytab.config.Config;
+import fr.flowsqy.easytab.config.ConfigLoader;
+import fr.flowsqy.easytab.group.GroupData;
 
 public class EasyTabPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        final var configLoader = new ConfigLoader();
+        final var dataFolder = getDataFolder();
+        final var logger = getLogger();
+        if (!configLoader.checkDataFolder(dataFolder)) {
+            Bukkit.getPluginManager().disablePlugin(this);
+            logger.warning("Can't write in the plugin directory. Disable the plugin");
+            return;
+        }
+        final Config config = new Config();
+        config.load(configLoader, this, "config.yml");
+        final Map<String, GroupData> groupDatas = config.loadGroupDatas(logger);
     }
 
 }
