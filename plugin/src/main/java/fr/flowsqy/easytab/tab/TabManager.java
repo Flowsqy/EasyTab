8dd47b45daddddba7ca1ae1411781e4785371674
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2024-12-20 18:35:32 +0100

    Implement GroupData comparator in TabManager

--- plugin/src/main/java/fr/flowsqy/easytab/tab/TabManager.java
+++ plugin/src/main/java/fr/flowsqy/easytab/tab/TabManager.java
@@ -23,10 +23,35 @@ public class TabManager {
     public TabManager() {
         lock = new ReentrantLock();
         nameTranslator = new NameTranslator(10, Comparator.comparing(String::valueOf, (s1, s2) -> s1.compareTo(s2)));
-        final Comparator<PlayerSnapshot> comparator = ((ps1, ps2) -> {
-            // TODO Compare groups
-        }).thenComparing(ps -> ps.profile().name());
+        final Comparator<PlayerSnapshot> comparator = new GroupsComparator().thenComparing(ps -> ps.profile().name());
         list = new SortedLinkedList<>(comparator, nameTranslator.getMaxValue());
+    }
+
+    private static class GroupsComparator implements Comparator<PlayerSnapshot> {
+        @Override
+        public int compare(PlayerSnapshot ps1, PlayerSnapshot ps2) {
+            final int maxIndex;
+            final int result;
+            final GroupData[] groups1 = ps1.groups(), groups2 = ps2.groups();
+            final int l1 = groups1.length, l2 = groups2.length;
+            if (l1 < l2) {
+                maxIndex = l1;
+                result = 1;
+            } else if (l2 < l1) {
+                maxIndex = l2;
+                result = -1;
+            } else {
+                maxIndex = l1;
+                result = 0;
+            }
+            for (int index = 0; index < maxIndex; index++) {
+                final int comp = Integer.compare(-groups1[index].priority(), -groups2[index].priority());
+                if (comp != 0) {
+                    return comp;
+                }
+            }
+            return result;
+        }
     }
 
     public void add(@NotNull Player player, @NotNull GroupData[] groups) {
