cbe0fd1e14bfbee55597d46c84dc9e42bbd1eb31
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2024-12-22 13:35:24 +0100

    Add TeamUpdate

---
+++ plugin/src/main/java/fr/flowsqy/easytab/tab/TeamUpdate.java
@@ -0,0 +1,7 @@
+package fr.flowsqy.easytab.tab;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public record TeamUpdate(@Nullable String previousName, @Nullable String newName, @NotNull PlayerProfile profile) {
+}
