94cac49f7dff636aba23e3c2dc42fe269644f19b
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2024-12-22 15:32:04 +0100

    Add ScoreboardManager

---
+++ plugin/src/main/java/fr/flowsqy/easytab/tab/ScoreboardManager.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.easytab.tab;
+
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ScoreboardManager {
+
+    public void executeChanges(@NotNull List<TeamUpdate> updates) {
+    }
+
+}
